25205b147349321bbe18b9144a5dbf2dba2e1429
quantumexchange8 <96815929+quantumexchange8@users.noreply.github.com>
2025-05-02 00:17:09 +0800

    color

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,5 +1,5 @@
-<header class="header-image ken-burn-center light" style="z-index:9999;" data-parallax="true" data-natural-height="500" data-natural-width="1920" data-bleed="0" data-image-src="{{ $headerImage }}" data-offset="0">
-    <div class="container">
+<header class="header-image ken-burn-center light" data-parallax="true" data-natural-height="500" data-natural-width="1920" data-bleed="0" data-image-src="{{ $headerImage }}" data-offset="0">
+    <div class="container blur-background">
         <h1>{{ $title }}</h1>
         {{-- <h2>OUr core values </h2> --}}
         <ol class="breadcrumb">
